98c297637b6281d6df3f3ec7fd5d7a6d940a161b
sadlywithu <164394992+sadlywithu@users.noreply.github.com>
2024-12-12 12:31:05 +0300

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -1,26 +1,26 @@
 'use strict'
 
-let requestUrl = 'https://api.open-meteo.com/v1/forecast?latitude=56.3269&longitude=44.0059&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m';
-function getWeatherData() {
 
+function getWeatherData(latitude, longitude) {
+    let requestUrl = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`;
     let xhr = new XMLHttpRequest();
 
     xhr.open('GET', requestUrl);
 
     xhr.onload = () => {
+ 
         let xhr_parse = JSON.parse(xhr.response);
-
         console.log(xhr_parse);
 
-        let temperatureElement = document.getElementById('temperature');
+        console.log(`Широта: ${xhr_parse.latitude}, Долгота: ${xhr_parse.longitude}`);
 
+        let temperatureElement = document.getElementById('temperature');
         temperatureElement.textContent = `${xhr_parse.current.temperature_2m} °C`;
 
-        if(xhr_parse.current.temperature_2m > 0) {
-            let img = document.getElementById('img');
-            img.src = './sun.png';
-        }
-    }
+        viewCity(latitude, longitude);
+        
+    };
+
 
     xhr.send();
 
@@ -32,9 +32,41 @@ function getWeatherData() {
     document.getElementById('date').textContent = `${formattedDate}`;
 };
 
+function getGeolocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            let latitude = position.coords.latitude;
+            let longitude = position.coords.longitude;
+            getWeatherData(latitude, longitude);
+        }, error => {
+            console.error('Ошибка получения геолокации:', error.message);
+        });
+    } else {
+        console.error('Геолокация не поддерживается вашим браузером.');
+    }
+}
+
+function viewCity(latitude, longitude) {
+    let cityUrl = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json`;
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', cityUrl);
+
+    xhr.onload = () => {
+
+        let locationData = JSON.parse(xhr.response);
+        console.log(locationData);
+            
+        let cityElement = document.getElementById('city');
+        cityElement.textContent = locationData.address.city || locationData.address.town || locationData.address.village || "Город не найден";
+    };
+
+    xhr.send();
+}
 
 
+getGeolocation();
+setInterval(getGeolocation, 300000);
 
-getWeatherData();
-setInterval(getWeatherData,30000)
+
 
